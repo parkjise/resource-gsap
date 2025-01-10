ca89470391a843fa6fd14cabcfd4228e880a739f
JS <>
2025-01-10 10:41:22 +0900

    0110

--- 1. Les bases de GSAP/timeline.js
+++ 1. Les bases de GSAP/timeline.js
@@ -6,11 +6,47 @@ const img3 = document.querySelector('.container-images img:nth-child(3)')
 const title = document.querySelector('.title')
 const txt = document.querySelector('.txt')
 
-const TL = gsap.timeline();
+const TL = gsap.timeline({
+    defaults:{
+        duration:1,
+        ease:"elastic"
+    },
+    repeat:-1,
+    repeatDelay:0.5,
+    yoyo:true,
+    paused:true// 멈춤
+    // onComplete:() => console.log("Complete"),
+    // onStart:() => console.log("Start")
+});
+
+// TL
+// .to(img1,{autoAlpha:1, y:0,duration:1})
+// .to(img2,{autoAlpha:1, y:0,duration:1})
+// .to(img3,{autoAlpha:1, y:0,duration:1},1)
+// .to(title,{autoAlpha:1, y:0,duration:1})
+// .to(txt,{autoAlpha:1, y:0,duration:1})
+
+// TL
+// .to(img1,{autoAlpha:1, y:0,duration:1})
+// .to(img2,{autoAlpha:1, y:0,duration:1},'>')
+// .to(img3,{autoAlpha:1, y:0,duration:1},'<')
+// .to(title,{autoAlpha:1, y:0,duration:1})
+// .to(txt,{autoAlpha:1, y:0,duration:1})
+
 
 TL
 .to(img1,{autoAlpha:1, y:0})
-.to(img2,{autoAlpha:1, y:0})
-.to(img3,{autoAlpha:1, y:0})
-.to(title,{autoAlpha:1, y:0})
-.to(txt,{autoAlpha:1, y:0})
+.to(img2,{autoAlpha:1, y:0,ease:"power4"},'-=0.75')
+.to(img3,{autoAlpha:1, y:0},'-=0.75')
+.to(title,{autoAlpha:1, y:0},'-=0.75')
+// .addLabel('endAnim')
+.add(() => {
+    console.log('Fin de timeline')
+})
+.to(txt,{autoAlpha:1, y:0},'-=0.75')
+
+setTimeout(() => {
+    TL.seek('endAnim')
+    // TL.seek(1)
+    TL.play()
+},3000)
